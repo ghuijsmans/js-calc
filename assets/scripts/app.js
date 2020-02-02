c78b70ce24cd09ac9a4af146d575f7f1cf7b50a5
ghuijsmans <huijsmansgijs@gmail.com>
2020-02-02 16:44:00 +0100

    corrected mistake(s)

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,44 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-let calculationDescription = '(' + defaultResult + ')';
+function getUserInput() {
+    return parseInt(userInput.value);
+}
 
-outputResult(currentResult, calculationDescription);
+function creatAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
+function add() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    creatAndWriteOutput('+', initialResult, enteredNumber)
+}
+
+function subtract() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    creatAndWriteOutput('-', initialResult, enteredNumber)
+}
+
+function multiply() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    creatAndWriteOutput('*', initialResult, enteredNumber)
+}
+
+function divide() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    creatAndWriteOutput('/', initialResult, enteredNumber)
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
