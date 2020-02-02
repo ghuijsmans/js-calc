cd55e333823ddae90f05549c493c75a6b87fa768
ghuijsmans <huijsmansgijs@gmail.com>
2020-02-02 16:52:15 +0100

    merge conflict resolution

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,61 +1,57 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-let history = [0,2,6,8];
+let history = [0, 2, 6, 8];
 
 function getUserInput() {
-    return parseInt(userInput.value);
+  return parseInt(userInput.value);
 }
 
 function creatAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-    outputResult(currentResult, calcDescription);
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
 }
 
 function add() {
-    const enteredNumber = getUserInput();
-    const initialResult = currentResult;
-<<<<<<< HEAD
-    currentResult += enteredNumber;
-=======
-    currentResult = currentResult + +enteredNumber;
->>>>>>> 8631a8a48900cda91b03002a3cd8949fa14e1a4c
-    creatAndWriteOutput('+', initialResult, enteredNumber)
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+
+  currentResult = currentResult + +enteredNumber;
+  creatAndWriteOutput("+", initialResult, enteredNumber);
 }
 
 function subtract() {
-    const enteredNumber = getUserInput();
-    const initialResult = currentResult;
-<<<<<<< HEAD
-    currentResult -= enteredNumber;
-=======
-    currentResult = currentResult + +enteredNumber;
->>>>>>> 8631a8a48900cda91b03002a3cd8949fa14e1a4c
-    creatAndWriteOutput('-', initialResult, enteredNumber)
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+
+  currentResult -= enteredNumber;
+
+  currentResult = currentResult + +enteredNumber;
+
+  creatAndWriteOutput("-", initialResult, enteredNumber);
 }
 
 function multiply() {
-    const enteredNumber = getUserInput();
-    const initialResult = currentResult;
-<<<<<<< HEAD
-    currentResult *= enteredNumber;
-=======
-    currentResult = currentResult + +enteredNumber;
->>>>>>> 8631a8a48900cda91b03002a3cd8949fa14e1a4c
-    creatAndWriteOutput('*', initialResult, enteredNumber)
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+
+  currentResult *= enteredNumber;
+
+  currentResult = currentResult + +enteredNumber;
+
+  creatAndWriteOutput("*", initialResult, enteredNumber);
 }
 
 function divide() {
-    const enteredNumber = getUserInput();
-    const initialResult = currentResult;
-<<<<<<< HEAD
-    currentResult /= enteredNumber;
-=======
-    currentResult = currentResult + +enteredNumber;
->>>>>>> 8631a8a48900cda91b03002a3cd8949fa14e1a4c
-    creatAndWriteOutput('/', initialResult, enteredNumber)
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+
+  currentResult = currentResult + +enteredNumber;
+  creatAndWriteOutput("/", initialResult, enteredNumber);
 }
 
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
